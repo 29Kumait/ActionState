7e1ef972069e96f3dbea97042e21844c8f146357
Kumait <Kumait@gmail.com>
2024-04-09 13:06:06 +0200

    retouch

--- client/src/Pages/PageLogin.jsx
+++ client/src/Pages/PageLogin.jsx
@@ -1,11 +1,12 @@
 import Log from "../Components/New/Log.jsx";
+import main from "../assets/main.svg";
 const PageLogin = () => {
   return (
-      <div>
-        <Log />
-      </div>
+    <div>
+      <Log />
+      <img src={main} alt="main" />
+    </div>
   );
 };
 
 export default PageLogin;
-
