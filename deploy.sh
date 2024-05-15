0309cce5fd73f6697608b71b61a9ac189b850bf2
Kumait Mustafa <Kumait@gmail.com>
2024-05-15 00:23:36 +0000

    Commit Changes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-# Example deployment script
 echo "Starting deployment to development environment..."
-
-# Copy files to the server or run deployment commands
-# e.g., scp -r build/* user@dev-server:/var/www/html/
 
 echo "Deployment completed successfully."
 
